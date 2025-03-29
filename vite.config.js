c1f2e1723161f8db73a508f7c3d53c2100de8876
rakibhossain7273 <rakibhossain72730@gmail.com>
2025-03-29 17:19:46 +0000

    sulbd

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { resolve} from "path";
+import { defineConfig } from "vite";
+import { Script } from "vm";
+export default defineConfig({
+    build:{
+        rollupOptions:{
+            input:{
+                main:resolve(__dirname, "index.html"),
+                Script:resolve(__dirname, "script.js"),
+            },
+        },
+    },
+});
